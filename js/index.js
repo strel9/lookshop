9473546f88f999e1eb46ab0a7d6b2d479be89bae
strel9 <strel9@gmail.com>
2020-01-24 12:28:51 +0200

    cart counter, json file

--- js/index.js
+++ js/index.js
@@ -1,41 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
     // const search = document.querySelector('.search');
     const cartBtn = document.getElementById('cart');
-    const cart = document.querySelector('.cart');
     const goodsWrapper = document.querySelector('.goods-wrapper');
+    const cart = document.querySelector('.cart');
 
-    const createCardGoods = () => {
+    const createCardGoods = (id, title, brand, price, img) => {
         const card = document.createElement('div');
+        // ./img/girl_2.jpg
         card.className = 'card col-12 col-md-6 col-lg-4 col-xl-3';
         card.innerHTML = `<div class="card">
-                                <img class="" src="./img/girl_2.jpg">
-                                <p>Atmos&Here Shopper</p>
-                                <p class="card-price">$49.95</p>
-                                <a class="btn" href="#" role="button">Add to card <span><i class="fas fa-plus"></i></span></a>
+                                <img class="" src="${img}" alt="${title}">
+                                <button class="card-add-wishlist"
+                                data-goods-id ="${id}"><i class="far fa-heart"></i>
+                                </button>
+                                <p>${brand}</p>
+                                <p>${title}</p>
+                                <p class="card-price">${price}<i class="fas fa-hryvnia"></i></p>
+                                <button class="btn card-add-cart" role="button"
+                                data-goods-id ="${id}">Добавить в корзину <span><i class="fas fa-plus"></i></span></button>
                             </div>`;
         // console.log(card);
         return card;
     };
 
-    goodsWrapper.appendChild(createCardGoods());
-    goodsWrapper.appendChild(createCardGoods());
-    goodsWrapper.appendChild(createCardGoods());
-    goodsWrapper.appendChild(createCardGoods());
-    goodsWrapper.appendChild(createCardGoods());
 
-    const closeCart = () => {
-        cart.style.display = 'none';
+    // goodsWrapper.appendChild(createCardGoods(1, 'платье-1', 'adidas', 2000, 'img/girl_2.jpg'));
+    // goodsWrapper.appendChild(createCardGoods(2, 'платье-2', 'adidas', 3000, 'img/girl_3.jpg'));
+    // goodsWrapper.appendChild(createCardGoods(3, 'платье-3', 'adidas', 4000, 'img/girl_6.jpg'));
+
+    const closeCart = (event) => {
+        const target = event.target;
+        if (target === cart || target.classList.contains('cart-close')) {
+            cart.style.display = 'none';
+        }
+
     };
 
     const openCart = () => {
+        event.preventDefault();
         cart.style.display = 'flex';
 
+    };
+
+    const renderCard = (items) => {
+        items.forEach((item) => {
+            const { id, title, brand, price, imgCart } = item;
+            goodsWrapper.append(createCardGoods(id, title, brand, price, imgCart));
+            // console.log(item);
+        })
+    };
+
+    const getGoods = (handler) => {
+        fetch('db/db.json')
+            .then((response) => response.json())
+            // .then((data) => console.log(data));
+            .then(handler);
     };
 
     cartBtn.addEventListener('click', openCart);
     cart.addEventListener('click', closeCart);
 
-
+    getGoods(renderCard);
 
 });
 
