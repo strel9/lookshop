e03c6a45b9f40bebc54a4a47ffc7fb7b11ceca1b
strel9 <strel9@gmail.com>
2020-01-30 05:22:42 +0200

    brand place

--- js/index.js
+++ js/index.js
@@ -358,3 +358,24 @@ $(window).scroll(function () {
     };
 });
 
+// меню эффект
+document.querySelectorAll('.ba-menu__link').forEach((elem) => {
+
+    elem.onmouseenter =
+        elem.onmouseleave = (e) => {
+
+            const tolerance = 10
+
+            const left = 0
+            const right = elem.clientWidth
+
+            let x = e.pageX - elem.offsetLeft
+
+            if (x - tolerance < left) x = left
+            if (x + tolerance > right) x = right
+
+            elem.style.setProperty('--x', `${x}px`)
+
+        }
+
+})
